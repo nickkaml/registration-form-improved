646cf2cfbbae6821f7e3d30cfb1444369e9c62e0
Nikolai Kamilatov <nicko1608@yandex.ru>
2022-11-21 00:37:09 +0400

    first commit

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,86 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        //Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void studentForm() {
+
+        String firstName = "Иван";
+        String lastName = "Иванов";
+        String email = "ivan@mail.ru";
+        String phoneNumber = "1234567890";
+        String address = "улица Ленина, 35, кв. 1";
+
+
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+
+        $(byText("Male")).click();
+
+        $("#userNumber").setValue(phoneNumber);
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__month-select").selectOption("January");
+        $(byText("1")).click();
+
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+
+        $(byText("Sports")).click();
+        $(byText("Music")).click();
+
+        $("#uploadPicture").uploadFile(new File("src/test/resources/picture.jpeg"));
+
+        $("#currentAddress").setValue(address);
+        $("#state").click();
+        $("#state").$(byText("Rajasthan")).click();
+        $("#city").click();
+        $("#city").$(byText("Jaiselmer")).click();
+        $("#submit").click();
+
+
+        $(".table-responsive").shouldBe(Condition.visible);
+        $(".table-responsive").shouldHave(
+                text(firstName),
+                text(lastName),
+                text(email),
+                text("Male"),
+                text(phoneNumber),
+                text("01 January,2000"),
+                text("English, Maths"),
+                text("Sports, Music"),
+                text("picture.jpeg"),
+                text(address),
+                text("Rajasthan Jaiselmer")
+        );
+    }
+
+    @AfterAll
+    static void afterAll() {
+        Configuration.holdBrowserOpen = false;
+    }
+}
