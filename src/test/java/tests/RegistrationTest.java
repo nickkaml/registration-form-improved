a5818b85f7c7a06f355475196e67d861c3c98518
Nikolai Kamilatov <nicko1608@yandex.ru>
2022-11-21 01:40:56 +0400

    update successfulRegistrationTest()

--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -2,12 +2,12 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -16,18 +16,19 @@ public class RegistrationTest {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         //Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
-    void studentForm() {
+    void successfulRegistrationTest() {
 
         String firstName = "Ivan";
         String lastName = "Ivanov";
         String email = "ivan@mail.ru";
-        String phoneNumber = "1234567890";
+        String gender = "Male";
+        String phoneNumber = "0987654321";
         String address = "prospekt Mira, 1, flat 1";
 
 
@@ -42,22 +43,20 @@ public class RegistrationTest {
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
 
-        $(byText("Male")).click();
+        $("#genterWrapper").$(byText(gender)).click();
 
         $("#userNumber").setValue(phoneNumber);
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("2000");
         $(".react-datepicker__month-select").selectOption("January");
-        $(byText("1")).click();
+        $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
 
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#subjectsInput").setValue("Maths").pressEnter();
 
-        $(byText("Sports")).click();
-        $(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
 
-        $("#uploadPicture").uploadFile(new File("src/test/resources/picture.jpeg"));
+        $("#uploadPicture").uploadFromClasspath("img/picture.jpeg");
 
         $("#currentAddress").setValue(address);
         $("#state").click();
@@ -67,24 +66,20 @@ public class RegistrationTest {
         $("#submit").click();
 
 
-        $(".table-responsive").shouldBe(Condition.visible);
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(
                 text(firstName),
                 text(lastName),
                 text(email),
-                text("Male"),
+                text(gender),
                 text(phoneNumber),
                 text("01 January,2000"),
-                text("English, Maths"),
-                text("Sports, Music"),
+                text("English"),
+                text("Sports"),
                 text("picture.jpeg"),
                 text(address),
                 text("Rajasthan Jaiselmer")
         );
     }
-
-    /*@AfterAll
-    static void afterAll() {
-        Configuration.holdBrowserOpen = false;
-    }*/
 }
